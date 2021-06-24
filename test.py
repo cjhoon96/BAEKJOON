c8c6350e8fd5edf0a3038323b677749e1304cc5f
cjhoon96 <cjhoon96@gmail.com>
2021-06-25 03:24:48 +0900

    구현

--- test.py
+++ test.py
@@ -1,19 +1,7 @@
-def solution(v):
-    x_set = set()
-    y_set = set()
-    for x,y in v:
-        print(x,y)
-        if x in x_set:
-            x_set.remove(x)
-        else:
-            x_set.add(x)
-        if y in y_set:
-            y_set.remove(y)
-        else:
-            y_set.add(y)
-    
-    answer = [x_set.pop(),y_set.pop()]
+import sys
+input = sys.stdin.readline
+a = input()
+b = input()
 
-    return answer
-
-print(solution([[1, 1], [2, 2], [1, 2]]))
+a = tuple(input().split())
+print(a)
