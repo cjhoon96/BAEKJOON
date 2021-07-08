17305a5ba44932d2f7e665c6ccea65b553a11e9f
cjhoon96 <cjhoon96@gmail.com>
2021-07-09 00:03:09 +0900

    DP

--- DP/(DP)11055 가장 큰 증가 부분 수열.py
+++ DP/(DP)11055 가장 큰 증가 부분 수열.py
@@ -37,3 +37,15 @@ for i in range(N):
             dp[i] = max((dp[j] + A[i], dp[i]))
 
 print(max(dp))
+
+
+
+'''
+n = int(input())
+a = list(map(int, input().split()))
+c = [0] * 1001
+for i in range(n): 
+    c[a[i]] = max(c[:a[i]]) + a[i]
+    print(c)
+print(max(c))
+'''
--- test.py
+++ test.py
@@ -1,45 +1,10 @@
-import heapq
-import sys
-input = sys.stdin.readline
-
-N, M, A, B, C = map(int,input().split())
-
-graph = [[] for _ in range(N + 1)]
-
-for _ in range(M):
-    a, b, c = map(int,input().split())
-    graph[a].append((b,c))
-    graph[b].append((a,c))
-
-arrived = False
-
-visited = set([A])
-q = []
-
-q.append((0, 0, A, visited))
-
-while q:
-    shame, t_c, now, n_visited= heapq.heappop(q)
-
-    if now == B:
-        arrived = True
-        Shame = shame
-        break
-
-    for next, cost in graph[now]:
-        if next in n_visited:
-            continue
-        if t_c + cost > C:
-            continue
-        
-        heapq.heappush(q, (max((shame, cost)), t_c + cost, next, n_visited|set([next])))
-        print(list(q))
-
-if arrived:
-    print(Shame)
-
-else:
-    print(-1)
+n = int(input())
+a = list(map(int, input().split()))
+c = [0] * 1001
+for i in range(n): 
+    c[a[i]] = max(c[:a[i]]) + a[i]
+    print(c)
+print(max(c))
 
 '''
 5 5 1 5 10
