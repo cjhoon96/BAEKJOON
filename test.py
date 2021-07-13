7b6549644c77bd3eacd1032ba6ce8697dbbc7bd7
cjhoon96 <cjhoon96@gmail.com>
2021-07-14 01:25:33 +0900

    Bruteforcing

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
-def make_bi(a):
-    bi = ''
-    while a != 0:
-        bi += str(a % 2)
-        a //= 2
-    bi = bi[::-1]
-    lenth = len(bi)
-
-    if lenth < 8:
-        bi = '0' * (8 - lenth) + bi
-
-    return bi
-
-print(make_bi(5110699119940114000))
-
+a = 'abcdefghijklmnopqrstuvwxyz'
+b = input()
+for i in a:
+    if i in b:
+        print(b.count(i), end = ' ')
+    else:
+        print(-1, end = ' ')
 '''
 5 5 1 5 10
 1 2 5
