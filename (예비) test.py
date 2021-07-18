9ce8d008c1bb9c0be5267e41ac66d32f4625bff2
cjhoon96 <cjhoon96@gmail.com>
2021-07-19 03:19:54 +0900

    Divide and Conquer

--- (예비) test.py
+++ (예비) test.py
@@ -1,24 +1,66 @@
 import sys
 input = sys.stdin.readline
+from itertools import chain
 
-n, m = map(int,input().split())
+def list_sum(lst):
+    s = 0
+    for i in lst:
+        s += sum(i)
+    return s
 
-off_brand = []
+n = int(input())
+square = list(list(map(int,input().split())) for _ in range(n))
+frst_check = list_sum(square)
+if frst_check == n ** 2:
+    print(0)
+    print(1)
+elif frst_check == 0:
+    print(1)
+    print(0)
 
-lst = []
+else:
+    paper = []
+    paper.append(square)
 
-for _ in range(n):
-    lst.append(input().rstrip())
+    b_cnt = 0
+    w_cnt = 0 
+    while paper:
+        new_paper = []
+        n //= 2
+        if n == 1:
+            print(paper)
+            paper = list(chain(*list(chain(*paper))))
+            print(paper)
+            for i in paper:
+                if i:
+                    b_cnt += 1
+                else:
+                    w_cnt += 1
+            break
+        area = n ** 2
 
-for _ in range(m):
-    now = input().rstrip()
+        for square in paper:   
+            temp = [[], [], [], []]
+            for y in range(n):
+                temp[0].append(square[y][:n])
+                temp[1].append(square[y][n:])
+            for y in range(n,2*n):
+                temp[2].append(square[y][:n])
+                temp[3].append(square[y][n:])
+            
+            for new_square in temp:
+                color = list_sum(new_square)
+                if color == area:
+                    b_cnt += 1
+                elif color == 0:
+                    w_cnt += 1
+                else:
+                    new_paper.append(new_square)
+        paper = new_paper
+        
+        for i in paper:
+            print(i)
+        print(w_cnt, b_cnt)
 
-    if now in lst:
-        off_brand.append(now)
-
-off_brand.sort()
-
-print(len(off_brand))
-
-for i in off_brand:
-    print(i)
+    print(w_cnt)
+    print(b_cnt)
