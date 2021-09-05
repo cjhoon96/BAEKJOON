60ca2e0a8c44a62159f28de806ad618865f709dc
cjhoon96 <cjhoon96@gmail.com>
2021-09-05 23:19:24 +0900

    WEB

---
+++ ToDoList/button.js
@@ -0,0 +1,11 @@
+let button = document.getElementById('button');
+let input = document.getElementById('input');
+let list = document.getElementById('list');
+
+button.addEventListener('click', addSchedule);
+
+function addSchedule(){
+    let temp = document.createElement('li')
+    temp.innerHTML = input.ariaValueMax;
+    list.appendChild(temp)
+}
