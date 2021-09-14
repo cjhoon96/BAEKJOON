01f959603da5cb0293e947ecef812d82764c5664
cjhoon96 <cjhoon96@gmail.com>
2021-09-14 23:32:32 +0900

    WEB

--- ToDoList/button.js
+++ ToDoList/button.js
@@ -2,20 +2,23 @@ let button = document.getElementById('button');
 let input = document.getElementById('input');
 let list = document.getElementById('list');
 let schedules = [];
+let unassigned = [];
+
+
 
 input.addEventListener('click', function(){
-    let target = this.value
-    let targetStyle = this.style
-    if(target === "HH MM Contents"){
-        this.value = '';
-        targetStyle.color="black";
-    }
+	let target = this.value
+	let targetStyle = this.style
+	if(target === "HH MM Contents"){
+		this.value = '';
+		targetStyle.color="black";
+	}
 })
 
 input.addEventListener('keydown', function(e){
-    if(e.key === 'Enter'){
-        addSchedule();
-    }
+	if(e.key === 'Enter'){
+		addSchedule();
+	}
 })
 
 
@@ -23,14 +26,23 @@ input.addEventListener('keydown', function(e){
 button.addEventListener('click', addSchedule);
 
 function addSchedule(){
-    if(input.value != "HH MM Contents"){
-        schedules.push(input.value);
-        schedules.sort()
-        
-        
-        list.appendChild(temp);
-        input.value = '';
-    }
+	let target = input.value 
+	if(target != "HH MM Contents"){
+		let data = target.split(" ");
+		if (Number.isinteger(data[0]) && Number.isinteger(data[1])){
+			let H = data[0];
+			let M = data[1];
+			let action = data.slice(2).join(' ');
+			let schedule = [H,M,action];
+			schedules.push(schedule);
+			schedules.sort();
+		} else {
+			unassigned.push(target);
+		}
+
+		list.appendChild(temp);
+		input.value = '';
+	}
 }
 
 
