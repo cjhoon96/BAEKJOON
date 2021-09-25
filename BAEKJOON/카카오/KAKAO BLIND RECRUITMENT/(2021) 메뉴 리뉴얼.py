4adbd267653f89fc5d9f58ccdbcea26f9b06d4fc
cjhoon96 <cjhoon96@gmail.com>
2021-09-25 23:25:21 +0900

    KAKAO

--- BAEKJOON/카카오/KAKAO BLIND RECRUITMENT/(2021) 메뉴 리뉴얼.py
+++ BAEKJOON/카카오/KAKAO BLIND RECRUITMENT/(2021) 메뉴 리뉴얼.py
@@ -1,120 +1,24 @@
-'''
-    for i in range(n - 1):
-        std = orders[i]
-        
-        if len(std) < l:
-            continue
-            
-        for menu in combinations(std):
-            added = False
-            check = []
-            if menu in answer:
-                continue
-                
-            for j in range(i + 1, n):
-                for food in menu:
-                    if food not in orders[j]:
-                        break
-                else:
-                    if added:
-                        check.append(j)
-                    else:
-                        check.extend([list(menu), i, j])
-                        answer.append(menu)
-            
-            if check:
-                now_set.append(check)
-    print(now_set)
-    for l in course:
-        new_set = []
-        for last in now_set:
-            last_menu = last[0]
-            addition = l - len(last_menu)
-            length = len(last)
-            for i in range(1, length - 1):
-                idx = last[i]
-                std = order[idx].copy()
-                if len(std) < l:
-                    continue
-                for f in last_menu:
-                    std.remove(f)
-                for new_menu in combinations(std, addition):
-                    new_menu += last_menu
-                    added = False
-                    check = []
-                    if new_menu in answer:
-                        continue
-
-                    for j in range(i + 1, length):
-                        jdx = last[j]
-                        std_2 = orders[jdx]
-                        for food in menu:
-                            if food not in std_2:
-                                break
-                        else:
-                            if added:
-                                check.append(jdx)
-                            else:
-                                check.extend([list(new_menu), idx, jdx])
-                                answer.append(new_menu)
-                    if check:
-                        new_set.append(check)
-        
-        if new_set:
-            now_set = new_set
-        
-        if not now_set:
-            break
-    
-    return'''
-
-
 from itertools import combinations
 
 def solution(orders, course):
-    now_set = []
     
     n = len(orders)
     for i in range(n):
-        orders[i] = list(orders[i])
-
+        orders[i] = sorted(list(orders[i]))
+        
     answer = []
     
-    l = course[0]
-    
-    for i in range(n - 1):
-        std = orders[i]
-        
-        if len(std) < l:
-            continue
-            
-        for menu in combinations(std, l):
-            added = False
-            check = []
-            if menu in answer:
-                continue
-                
-            for j in range(i + 1, n):
-                for food in menu:
-                    if food not in orders[j]:
-                        break
-                else:
-                    if added:
-                        check.append(j)
-                    else:
-                        check.extend([list(menu), i, j])
-                        answer.append(menu)
-                        added = True
-            
-            if check:
-                now_set.append(check)
-    
+    now_set = [[[]] + [i for i in range(n)]]
     for l in course:
-        new_set = []
+        new_set = []        
+        maxi = 0
+        ap = []
+        visited = []
         for last in now_set:
             last_menu = last[0]
             addition = l - len(last_menu)
             length = len(last)
+
             for i in range(1, length - 1):
                 idx = last[i]
                 std = orders[idx].copy()
@@ -125,16 +29,18 @@ def solution(orders, course):
                 for new_menu in combinations(std, addition):
                     new_menu = list(new_menu) + last_menu
                     new_menu.sort()
-                    added = False
+                    added = 0
                     check = []
-                    if new_menu in answer:
+                    if new_menu in visited:
                         continue
 
+                    visited.append(new_menu)
                     for j in range(i + 1, length):
-                        print(last[j])
                         jdx = last[j]
                         std_2 = orders[jdx]
-                        for food in menu:
+                        if len(std_2) < l:
+                            continue
+                        for food in new_menu:
                             if food not in std_2:
                                 break
                         else:
@@ -142,40 +48,22 @@ def solution(orders, course):
                                 check.append(jdx)
                             else:
                                 check.extend([list(new_menu), idx, jdx])
-                                answer.append(new_menu)
-                    if check:
+                            added += 1
+                    if added:
                         new_set.append(check)
-        
+                        if maxi < added:
+                            maxi = added
+                            ap = [''.join(new_menu)]
+                        elif maxi == added:
+                            ap.append(''.join(new_menu))
+                            
         if new_set:
             now_set = new_set
-        
+            answer += ap
+
         if not now_set:
             break
-    
+
+        answer.sort()
     return answer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
