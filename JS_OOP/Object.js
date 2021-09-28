cca3317d6b89eb4738a6c97fafb129b1be292ea7
cjhoon96 <cjhoon96@gmail.com>
2021-09-29 03:30:35 +0900

    WEB

--- JS_OOP/Object.js
+++ JS_OOP/Object.js
@@ -27,6 +27,34 @@ let Grade = {
 }
 
 
+
+// 생성자 Constructure - 객체를 찍어내는 공장
 let d1 = new Date('2021-9-28');
 console.log('d1.getFullYear()', d1.getFullYear());
 console.log('d1.getMonth()', d1.getMonth());				//0부터 카운트 되어 8이 출력
+
+
+function Score(name, a, b, c){
+	this.name = name;
+	this.first = a;
+	this.second = b;
+	this.third = c;
+	this.sum = function(){
+		return this.first+this.second+this.third;
+	}
+}
+
+
+// new를 통해 Chae 를 객체로 만들어준다 
+let Chae = new Score('Jihoon', 10, 20, 30);
+
+console.log(Chae.name, '\'s average score is', Chae.sum()/3);
+
+// 장점
+// 기존에는 객체를 생성할때마다 새롭게 정의를 해줘야 했으나 construct function을 실행 함으로서 빠르게 객체를 생성할 수 있으며 수정할 수 있다.
+
+//Prototype
+//javascript = prototype based language
+
+//생성자 안에서 메소드를 만드는 단점: 데이터 소모량이 많아 생산성이 떨어짐
+//prototype을 통해 생산성을 높여준다
