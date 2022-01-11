446a59f3e99036967cc86c48a1cb1b1b5734677c
cjhoon96 <cjhoon96@gmail.com>
2022-01-11 19:12:30 +0900

    SAP

---
+++ 혁신성장 청년인재 집중양성 7기 sap 클라우드/7. Web Development Basic (JavaScript, HTML5 and CSS 3)/calc.js
@@ -0,0 +1,59 @@
+var aInput = document.getElementById('a');
+var bInput = document.getElementById('b');
+var opInput = document.getElementById('op');
+var calcBtn = document.getElementById('calc');
+var resetBtn = document.getElementById('reset');
+var rslt = document.getElementById('rslt');
+var Inputs = [aInput, bInput, opInput];
+
+Inputs.map((Input)=>{
+  Input.addEventListener('keydown', function(oEvent){
+    if (oEvent.key === 'Enter'){
+      var emptyChck = chckInpt();
+      console.log(emptyChck);
+      if (emptyChck < 0){
+        rslt.innerText = calc(aInput.value, bInput.value, opInput.value);
+      } else {
+        Inputs[emptyChck].focus();
+      }
+    }
+  })
+})
+
+resetBtn.addEventListener('click', function(){
+  alert('click');
+  for (var i = 0 ; i < Inputs.length ; i++){
+    alert(i);
+    Inputs[i].value = '';
+  }
+  rslt.innerText = '';
+})
+
+
+function calc(a, b, op) {
+  a = parseFloat(a);
+  b = parseFloat(b);
+  switch (op) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return (b ? a / b : "0으로 나눌 수 없다");
+    case "%":
+      return (b ? a % b : "0으로 나눌 수 없다");
+  }
+}
+
+
+function chckInpt(){
+  for (var i = 0 ; i < Inputs.length ; i++){
+    console.log(Inputs[i].value);
+    if (Inputs[i].value === ''){
+      return i;
+    }
+  }
+  return -1;
+}
---
+++ 혁신성장 청년인재 집중양성 7기 sap 클라우드/7. Web Development Basic (JavaScript, HTML5 and CSS 3)/text.js
@@ -0,0 +1,4 @@
+var fruit = ["Banana", "Orange", "Apple", "Mango"];
+
+var x = fruit.pop();
+console.log(x, fruit);
