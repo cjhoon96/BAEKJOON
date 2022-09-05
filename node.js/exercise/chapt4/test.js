cb7bba95ddba7cbe3da95a095f915ef5bd39351e
cjhoon96 <cjhoon96@gmail.com>
2022-09-05 23:55:52 +0900

    SAP Certi

---
+++ node.js/exercise/chapt4/test.js
@@ -0,0 +1,13 @@
+// section 1
+const parseCookies = (cookie = '') =>
+  cookie
+    .split(';')
+    .map(v => v.split('='))
+    .reduce((acc, [k, v]) => {
+      acc[k.trim()] = decodeURIComponent(v);
+      return acc;
+    }, {});
+
+  
+console.log(parseCookies('a=1;b=2;c=3'));
+
