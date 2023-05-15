973fb6446de45be4f94a6bbf9e82997a7f035a31
cjhoon96 <cjhoon96@gmail.com>
2023-05-16 00:20:39 +0900

    TypeScript

---
+++ TypeScript/Effective TypeScript/01.타입스크립트와 자바스크립트의 관계 이해하기/src/test.js
@@ -0,0 +1,20 @@
+// let city = 'new york city';
+// console.log(city.toUppercase());
+
+
+
+// const x = 2 + '3';
+// const y = '2' + 3;
+
+// console.log(x,y)
+
+// const a = null + 7
+// const b = [] + 12
+// alert('Hello', 'TypeScript');
+
+
+
+
+
+const names = ['Alice', 'Bob'];
+console.log(names[2].toUpperCase());
---
+++ TypeScript/Effective TypeScript/01.타입스크립트와 자바스크립트의 관계 이해하기/src/testcode.ts
@@ -0,0 +1,46 @@
+// let city = 'new york city';
+// console.log(city.toUppercase());
+
+
+
+// const states = [
+//     {name: 'Alabama', capital: 'Montgomery'},
+//     {name: 'Alaska', capital: 'Juneau'},
+//     {name: 'Arizona', capital: 'Phoenix'},
+// ];
+// for (const state of states) {
+//     console.log(state.capitol); // capital 에서 오타가 발생한 경우 
+// }
+
+
+
+// const states = [
+//     {name: 'Alabama', capitol: 'Montgomery'}, // 개체 리터럴은 알려진 속성만 지정할 수 
+//     {name: 'Alaska', capital: 'Juneau'},
+//     {name: 'Arizona', capital: 'Phoenix'},
+// ]
+// interface State {
+//     name: string;
+//     capital: string;
+// }
+// const states: State[] = [
+//     {name: 'Alabama', capitol: 'Montgomery'}, // 개체 리터럴은 알려진 속성만 지정할 수 
+//     {name: 'Alaska', capital: 'Juneau'},
+//     {name: 'Arizona', capital: 'Phoenix'},
+// ]
+
+
+
+// const x = 2 + '3';
+// const y = '2' + 3;
+
+
+
+// const a = null + 7
+// const b = [] + 12
+// alert('Hello', 'TypeScript');
+
+
+
+const names = ['Alice', 'Bob'];
+console.log(names[2].toUpperCase());
